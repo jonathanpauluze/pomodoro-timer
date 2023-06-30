5c845178c7647fd5f4a29d8ce854a31c53309d6b
Jonathan Pauluze <jonathanpauluze@gmail.com>
2023-06-30 11:07:46 -0300

    chore: applying theme

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -3,12 +3,13 @@ module.exports = {
   extends: [
     'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
-    'plugin:react-hooks/recommended',
+    'plugin:react-hooks/recommended'
   ],
   parser: '@typescript-eslint/parser',
   parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
   plugins: ['react-refresh'],
   rules: {
     'react-refresh/only-export-components': 'warn',
-  },
+    '@typescript-eslint/no-empty-interface': 'off'
+  }
 }
