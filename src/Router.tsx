0ad3984c9b7c5cca759deff72f417b3993ce1f48
Jonathan Pauluze <jonathanpauluze@gmail.com>
2023-06-30 11:43:35 -0300

    chore: setting routes up

---
+++ src/Router.tsx
@@ -0,0 +1,13 @@
+import { FunctionComponent } from 'react'
+import { Routes, Route } from 'react-router-dom'
+import { Home } from './pages/Home'
+import { History } from './pages/History'
+
+export const Router: FunctionComponent = () => {
+  return (
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/history" element={<History />} />
+    </Routes>
+  )
+}
---
+++ src/pages/History.tsx
@@ -0,0 +1,5 @@
+import { FunctionComponent } from 'react'
+
+export const History: FunctionComponent = () => {
+  return <h1>History</h1>
+}
